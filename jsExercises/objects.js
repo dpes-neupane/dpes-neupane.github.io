59c179e0b7a5507f4fd82221645490c46863e378
Dpes Neupane <dpesn47@gmail.com>
2021-12-21 15:09:29 +0545

    jsExercises: 4 of them finished

---
+++ jsExercises/objects.js
@@ -0,0 +1,29 @@
+const me = {
+    name: 'Dipesh',
+    address: 'Kirtipur',
+    emails: 'anonymous@gmail.com',
+    interests: ['eating', 'sleeping'],
+    education: [{
+            name: 'National Nepali School',
+            enrolledDate: 2001
+        },
+        {
+            name: 'International Nepali School',
+            enrolledDate: 2014
+        },
+        {
+            name: 'Patan Multiple Campus',
+            enrolledDate: 2018
+        }
+    ]
+}
+
+
+var result = Object.keys(me).forEach(key => {
+    if (key === "education") {
+        me[key].forEach(k => {
+            console.log("Name: " + k["name"] + ", Date: " + k["enrolledDate"]);
+        })
+    }
+
+});
