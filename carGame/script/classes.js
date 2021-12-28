3a147754c2b5fe6085cf178ad89d4e81d83c08d0
Dpes Neupane <dpesn47@gmail.com>
2021-12-28 20:28:22 +0545

    the game is build with features like loot and bullets

--- carGame/script/classes.js
+++ carGame/script/classes.js
@@ -1,3 +1,13 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+
+
 class HitBox {
     constructor(x, y, w, h) {
         this.x = x;
@@ -62,13 +72,58 @@ class Player {
         this.x -= 5;
     }
 
-    shootBullets() {
 
+
+
+}
+
+
+class Bullet {
+    constructor(x, y) {
+        this.x = x + 20;
+        this.y = y;
+        this.w = 5;
+        this.h = 10;
+        this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
+    }
+
+
+    moveBullet() {
+        this.y -= 10;
+        this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
+        this.hitbox.draw();
+        // console.log(this.y);
     }
 
 
 }
 
+
+class Loot {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = 100;
+        this.h = 100;
+        this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
+        this.image = new Image();
+        this.image.src = "./images/loot.png";
+    }
+    placeLoot() {
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+    drawHitBox() {
+        this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
+    }
+    moveLoot() {
+        this.y += 1;
+        this.drawHitBox();
+        this.placeLoot();
+
+
+    }
+
+}
 
 
 
@@ -80,7 +135,7 @@ class Adverseries {
         this.h = h;
         this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
         this.image = new Image();
-        this.image.src = "./images/police-car-ulta.png";
+        this.image.src = "./images/police-car.png";
     }
 
     placeAdverseries() {
@@ -89,11 +144,13 @@ class Adverseries {
 
     drawHitBox() {
         this.hitbox = new HitBox(this.x, this.y, this.w, this.h);
-        this.hitbox.draw();
+        // this.hitbox.draw();
 
     }
     moveAdverseries() {
         this.y += 1;
+        this.drawHitBox();
+        this.placeAdverseries();
 
 
     }
@@ -103,19 +160,28 @@ class Adverseries {
 
 
 
-class Game {
+class CarGame {
     constructor(noOfPlayer, backgroundImge) {
         this.noOfPlayer = noOfPlayer;
         this.backgroundImge = new Image();
         this.backgroundImge.src = backgroundImge;
         this.players = [];
         for (let i = 0; i < this.noOfPlayer; i++) {
-            this.players.push(new Player()); //add--feature: player coordinates for multiple players
+            let p = new Player();
+            p.drawHitBox();
+            p.placePlayer();
+            this.players.push(p);
+
+            //add--feature: player coordinates for multiple players
         }
         this.x = 0;
         this.y = 0;
         this.y2 = 10;
         this.adverseriesArr = [];
+        this.counter = 0;
+        this.divider = getRandomInt(210, 300);
+        this.lootArr = [];
+        this.loot = 0;
     }
     drawBackGround() {
         ctx.drawImage(this.backgroundImge, this.x, this.y, 500, 1000);
@@ -142,38 +208,214 @@ class Game {
     }
 
 
-    makeAdverseries() {
-        this.adverseries = new Adverseries(80, -50, 50, 101);
-        this.adverseriesArr.push(this.adverseries);
 
 
 
+    makeAdverseriesAndLoot() {
+        this.positionsX = [80, 230, 370];
+        let addLoot = false;
+        this.counter++;
+        if (this.counter % this.divider === 0) {
+            this.counter = 0;
+            this.loot++;
+            if (this.loot % getRandomInt(2, 8) == 0) {
+                this.loot = 0;
+                addLoot = true;
+            }
+            this.positionsX.forEach(element => {
+                let rInt = getRandomInt(0, 2);
+                if (rInt) {
+                    if (addLoot) {
+                        this.lootArr.push(new Loot(element, -201));
+                        console.log("loot");
 
-
-
-    }
-    moveAdverseries() {
-        if (this.adverseries == undefined) return;
-        this.adverseries.drawHitBox();
-        this.adverseries.placeAdverseries();
-        this.adverseries.moveAdverseries();
-        if (this.adverseries.y > canvas.height) {
-            delete this.adverseries;
-            console.log("adverseries deleted");
-            this.makeAdverseries();
+                    }
+                    this.adverseriesArr.push(new Adverseries(element, -101, 50, 101));
+                }
+            })
         }
+
+
     }
 
-    detectCollision(player, id) {
+
+
+    moveAdverseriesandLoot() {
+        this.adverseriesArr.forEach(element => {
+            element.moveAdverseries();
+
+        });
+
+        this.adverseriesArr = this.adverseriesArr.filter(element => {
+            return element.y < canvas.height;
+        });
+        this.lootArr.forEach(element => {
+            element.moveLoot();
+
+        });
+        this.lootArr = this.lootArr.filter(element => {
+            return element.y < canvas.height;
+        });
+        this.makeAdverseriesAndLoot();
+
+
+    }
+
+    detectCollision() {
         let collision = false;
         this.adverseriesArr.forEach(element => {
-            if (element.hitbox.intersects(player)) {
+            if (element.hitbox.intersects(this.players[0])) {
 
                 collision = true;
             }
 
         })
         return collision;
+
+    }
+    detectLoot() {
+        let collision = -1;
+        this.lootArr.forEach((element, index) => {
+            if (element.hitbox.intersects(this.players[0])) {
+
+                collision = index;
+
+            }
+
+        })
+        return collision;
+
+    }
+
+
+
+
+
+}
+
+
+
+
+class Game {
+    constructor() {
+        this.game = new CarGame(1, "./images/road.png");
+        this.animationId = 0;
+        this.right = false;
+        this.game.makeAdverseriesAndLoot();
+        this.collided = false;
+        this.bulletArr = [];
+        this.ammoLeft = 10;
+        this.shoot = false;
+        this.gotLoot = -1;
+        this.score = 0;
+    }
+
+    addEvents() {
+        window.addEventListener("keydown", (e) => {
+            switch (e.keyCode) {
+                case 37:
+                    this.game.players[0].currentPosition--;
+                    if (this.game.players[0].currentPosition < 0) {
+                        this.game.players[0].currentPosition = 0;
+                    }
+                    this.right = false;
+                    console.log("left");
+                    break;
+                case 39:
+                    this.game.players[0].currentPosition++;
+                    if (this.game.players[0].currentPosition > 2) {
+                        this.game.players[0].currentPosition = 2;
+                    }
+                    this.right = true;
+                    console.log("right");
+                    break;
+                case 32:
+                    console.log("space");
+                    if (this.ammoLeft > 0) {
+                        let b = new Bullet(this.game.players[0].x, this.game.players[0].y);
+                        this.bulletArr.push(b);
+                        this.shoot = true;
+                        this.ammoLeft -= 1;
+                    }
+            }
+        });
+    }
+    writeScore(score, ammo) {
+        score *= 10;
+        ctx.font = "30px Comic Sans MS";
+        ctx.strokeStyle = "red";
+        ctx.fillText(Math.floor(score), 50, 30);
+        ctx.fillText(ammo, 450, 30);
+
+    }
+
+    gameLoop() {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        this.game.loopBackGround();
+        this.game.moveAdverseriesandLoot();
+        this.collided = this.game.detectCollision();
+        this.gotLoot = this.game.detectLoot();
+
+        this.game.players.forEach(element => {
+            // element.drawHitBox();
+            element.placePlayer();
+            if (element.x < element.positions[element.currentPosition] && this.right) {
+                element.changePositionRight();
+            }
+            if (element.x > element.positions[element.currentPosition] && !this.right) {
+                element.changePositionLeft();
+            }
+        });
+
+        this.bulletArr.forEach(element => {
+            element.moveBullet();
+
+        })
+        this.bulletArr = this.bulletArr.filter(element => {
+            return !(element.y < 0);
+        });
+        for (let i = 0; i < this.bulletArr.length; i++) {
+            for (let j = 0; j < this.game.adverseriesArr.length; j++) {
+
+                if (this.bulletArr[i].hitbox.intersects(this.game.adverseriesArr[j])) {
+                    this.bulletArr.splice(i, 1);
+                    this.game.adverseriesArr.splice(j, 1);
+                    this.score += 1;
+                    break;
+
+                }
+            }
+        }
+        // 
+
+
+
+        if (this.gotLoot > -1) {
+            this.game.lootArr.splice(this.gotLoot, 1);
+            this.ammoLeft = 10;
+            this.gotLoot = -1;
+
+
+        }
+
+        if (this.collided) {
+            this.gameOver(this.animationId);
+            return;
+        }
+        this.score += 0.01;
+        this.writeScore(this.score, this.ammoLeft);
+
+        this.animationId = requestAnimationFrame(this.gameLoop.bind(this));
+
+    }
+
+
+    gameOver(id) {
+        this.writeScore(this.score, this.ammoLeft);
+        cancelAnimationFrame(id);
+
 
     }
 
--- carGame/script/script.js
+++ carGame/script/script.js
@@ -2,44 +2,53 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 600;
-let right = true;
-let move = false;
-const game = new Game(1, "./images/road.png");
-const player = new Player();
-game.makeAdverseries();
-let animationId = 0;
-let collided = false;
-
-window.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-        case 37:
-            console.log('left');
-            right = false;
-            move = true;
-            player.currentPosition--;
-            if (player.currentPosition < 0) {
-                player.currentPosition = 0;
-            }
-            break;
-            // case 38:
-            //     console.log('up');
-            //     break;
-        case 39:
-            console.log('right');
-            right = true;
-            move = true;
-            player.currentPosition++;
-            if (player.currentPosition > 2) {
-                player.currentPosition = 2;
-            }
+// let right = true;
+// let move = false;
+// const game = new Game(1, "./images/road.png");
+// const player = new Player();
+// game.makeAdverseries();
+// let animationId = 0;
+// let collided = false;
+// let shoot = false;
+// let bulletArr = [];
 
 
+// window.addEventListener("keydown", (e) => {
+//     switch (e.keyCode) {
+//         case 37:
+//             console.log('left');
+//             right = false;
+//             move = true;
+//             player.currentPosition--;
+//             if (player.currentPosition < 0) {
+//                 player.currentPosition = 0;
+//             }
+//             break;
+//             // case 38:
+//             //     console.log('up');
+//             //     break;
+//         case 39:
+//             console.log('right');
+//             right = true;
+//             move = true;
+//             player.currentPosition++;
+//             if (player.currentPosition > 2) {
+//                 player.currentPosition = 2;
+//             }
 
-            break;
-            // case 40:
-            //     console.log('down')
-    }
-});
+
+
+//             break;
+//         case 32:
+//             shoot = true;
+
+//             bullet = new Bullet(player.x, player.y);
+//             bulletArr.push(bullet);
+//             index++;
+
+//             console.log("space");
+//     }
+// });
 
 function init() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -57,7 +66,7 @@ function init() {
     }
 
     game.moveAdverseries();
-    collided = game.detectCollision(player, animationId);
+    collided = game.detectCollision(player);
     player.drawHitBox();
     player.placePlayer();
     if (collided) {
@@ -65,10 +74,17 @@ function init() {
         cancelAnimationFrame(animationId);
         return;
     }
+    if (shoot) {
 
+        bullet.moveBullet();
+    }
 
 
     animationId = requestAnimationFrame(init);
 }
 
-init();
+// init();
+
+const game = new Game();
+game.addEvents();
+game.gameLoop();
